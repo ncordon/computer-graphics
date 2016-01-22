ed3dfe04acda4b826f6be7682341f6884137d791
NCordon <nacho.cordon.castillo@gmail.com>
2016-01-22 01:11:11 +0100

    Añadida gestión de teclas. Falta por completar

--- srcs-prac/practica3.cpp
+++ srcs-prac/practica3.cpp
@@ -1,24 +1,43 @@
-// *********************************************************************
-// **
-// ** Informática Gráfica, curso 2015-16
-// ** Práctica 2  (declaraciones públicas)
-// **
-// *********************************************************************
-
-#ifndef IG_PRACTICA2_HPP
-#define IG_PRACTICA2_HPP
-//#include "MallaInd.hpp"
-#include "stdlib.h"
-#include "Mallas.hpp"
-#include <iostream>
-#include <cctype>
-
-using namespace std;
-
-void P3_Inicializar( int argc, char *argv[] ) ;
-bool P3_FGE_PulsarTeclaNormal(  unsigned char tecla ) ;
-void P3_DibujarObjetos( unsigned modo ) ;
+#include "Practica3.hpp"
 
 
 
-#endif
+// Grado de libertad actual seleccionado en el programa
+int p3_degree;
+
+
+
+
+void P3_Inicializar( int argc, char *argv[] ){
+    p3_degree = 0;
+
+}
+
+
+
+bool P3_FGE_PulsarTeclaNormal(  unsigned char tecla ){
+    bool redibuja = true;
+    
+    switch(toupper(tecla)){
+    case 'G':
+        p3_fdegree += 1;
+        p3_fdegree %= 3;
+
+        break;
+    case '>':
+        // Aumentar grado de libertad
+
+        // Si se sobrepasa el máximo, no redibujar
+        break;
+    case '<':
+
+        break;
+
+    default:
+        redibujar = false;        
+    }
+
+
+    
+    return redibujar;
+}
