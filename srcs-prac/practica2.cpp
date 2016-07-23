c68bea65c21e0dba25036bfd3f558ca21f894e92
NCordon <nacho.cordon.castillo@gmail.com>
2016-07-23 05:37:34 +0200

    Añadidos parámetros por defecto de inicialización en práctica 2

--- srcs-prac/practica2.cpp
+++ srcs-prac/practica2.cpp
@@ -16,15 +16,29 @@ const unsigned N_OBJETOS = 2;
 
 void P2_Inicializar( int argc, char *argv[]){
     // Crear objeto de tipo malla PLY y objeto de revolución con new.
+    string  archivo_ply,
+            archivo_rev;
+    unsigned n_perfiles;
 
-    if (argc != 4){
+    if (argc > 1 && argc < 4){
         cerr << "Número de argumentos incorrecto" << endl;
         cerr << "Uso: <nombre programa> <archivo PLY> <archivo PLY rev> <num copias perfil>" << endl;
         exit(1);
     }
+    else if(argc == 4){
+        archivo_ply = argv[1];
+        archivo_rev = argv[2];
+        n_perfiles = atoi(argv[3]);
+    }
+    // Parámetros por defecto para que pueda usarse directamente desde el make
+    else{
+        archivo_ply = "../plys/ant.ply";
+        archivo_rev = "../plys/peon.ply";
+        n_perfiles = 20;
+    }
 
-    obj = new MallaPLY(argv[1]);
-    obj_rev = new MallaRevol(argv[2], atoi(argv[3]));
+    obj = new MallaPLY(&archivo_ply[0]);
+    obj_rev = new MallaRevol(&archivo_rev[0], n_perfiles);
     // Objeto 0 = ply, objeto 1 = revolución
     p2_objeto_activo = 0;
 }
