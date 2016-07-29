2b699e9c63aa9ac6baae2a25488c2572e6460600
NCordon <nacho.cordon.castillo@gmail.com>
2016-07-29 15:03:53 +0200

    Brazo horizontal de la grúa implementado

--- srcs-prac/grua.cpp
+++ srcs-prac/grua.cpp
@@ -6,6 +6,14 @@ Viga::Viga(double longitud){
 }
 
 
+VigaDiagonal::VigaDiagonal(double longitud){
+    agregar(MAT_Traslacion(1,0,0));
+    agregar(MAT_Escalado(-1,1,1));
+    agregar(MAT_Rotacion(-45,0,0,1));
+    agregar(new Viga(longitud));
+}
+
+
 ParColumnas::ParColumnas(){
     agregar(new Viga);
     agregar(MAT_Traslacion(1,0,0));
@@ -14,16 +22,11 @@ ParColumnas::ParColumnas(){
 
 
 ArmazonBasico::ArmazonBasico(){
-    NodoEscena *viga_diagonal = new NodoEscena;
-    viga_diagonal->agregar(MAT_Traslacion(1,0,0));
-    viga_diagonal->agregar(MAT_Rotacion(45,0,0,1));
-    viga_diagonal->agregar(new Viga(sqrt(2)));
-
+    agregar(new VigaDiagonal(sqrt(2)));
     agregar(new ParColumnas);
     agregar(MAT_Traslacion(1,0,0));
     agregar(MAT_Rotacion(90,0,0,1));
     agregar(new ParColumnas);
-    agregar(viga_diagonal);
 }
 
 
@@ -59,23 +62,39 @@ BrazoVertical::BrazoVertical(unsigned num_cubos){
     agregar(new ParTiraTriangulada(num_cubos));
 }
 
+LateralBrazoHorizontal::LateralBrazoHorizontal(unsigned num_cubos){
+    agregar(new TiraTriangulada(num_cubos));
+    agregar(MAT_Traslacion(0,-1,0));
+    agregar(new VigaDiagonal(sqrt(2)));
+    agregar(MAT_Traslacion(1,0,0));
+    agregar(new Viga);
+}
+
 BrazoHorizontal::BrazoHorizontal(unsigned num_cubos){
-    unsigned num_cubos_horizontal = (int)(num_cubos*5.0/6);
     NodoEscena *un_lateral = new NodoEscena;
     NodoEscena *otro_lateral = new NodoEscena;
     NodoEscena *tapa = new NodoEscena;
     un_lateral->agregar(MAT_Rotacion(-30,0,1,0));
-    un_lateral->agregar(new TiraTriangulada(num_cubos_horizontal));
+    un_lateral->agregar(new LateralBrazoHorizontal(num_cubos));
     otro_lateral->agregar(MAT_Rotacion(30,0,1,0));
-    otro_lateral->agregar(new TiraTriangulada(num_cubos_horizontal));
-    tapa->agregar(MAT_Traslacion(sqrt(1-0.5*0.5),0,0.5));
+    otro_lateral->agregar(new LateralBrazoHorizontal(num_cubos));
+    tapa->agregar(MAT_Traslacion(sqrt(1-0.5*0.5),-1,0.5));
     tapa->agregar(MAT_Rotacion(90,0,1,0));
-    tapa->agregar(new TiraTriangulada(num_cubos_horizontal));
+    tapa->agregar(new TiraTriangulada(num_cubos+1));
 
-
-    agregar(MAT_Traslacion(-1.0*num_cubos_horizontal, num_cubos+sqrt(1-0.5*0.5),0.5));
+    agregar(MAT_Traslacion(-1.0*num_cubos,0,0.5));
     agregar(MAT_Rotacion(-90,0,0,1));
     agregar(un_lateral);
     agregar(otro_lateral);
     agregar(tapa);
+}
+
+RemateBrazoHorizontal::RemateBrazoHorizontal(unsigned num_cubos){
+    agregar(new Cilindro);
+}
+
+BaseGrua::BaseGrua(){
+    agregar(MAT_Traslacion(-0.25,0,-0.25));
+    agregar(MAT_Escalado(1.5,0.5,1.5));
+    agregar(new Cubo);
 }
--- srcs-prac/grua.hpp
+++ srcs-prac/grua.hpp
@@ -14,6 +14,12 @@ public:
 };
 
 
+class VigaDiagonal : public NodoEscena{
+public:
+    VigaDiagonal(double longitud = 1.0);
+};
+
+
 class ParColumnas : public NodoEscena{
 public:
     ParColumnas();
@@ -49,16 +55,39 @@ public:
     BrazoVertical(unsigned num_cubos);
 };
 
+
+class LateralBrazoHorizontal: public NodoEscena{
+public:
+    LateralBrazoHorizontal(unsigned num_cubos);
+};
+
+
 class BrazoHorizontal: public NodoEscena{
 public:
     BrazoHorizontal(unsigned num_cubos);
 };
 
+
+class RemateBrazoHorizontal : public NodoEscena{
+public:
+    RemateBrazoHorizontal(unsigned num_cubos);
+};
+
+class BaseGrua : public NodoEscena{
+public:
+    BaseGrua();
+};
+
 class Grua : public NodoEscena{
 public:
     Grua(){
-        agregar(new BrazoVertical(10));
-        agregar(new BrazoHorizontal(10));
+        unsigned num_cubos_vertical = 10;
+        unsigned num_cubos_horizontal = (int)(num_cubos_vertical*5.0/6);
+        agregar(new BaseGrua);
+        agregar(MAT_Traslacion(0,0.5,0));
+        agregar(new BrazoVertical(num_cubos_vertical));
+        agregar(MAT_Traslacion(0, num_cubos_vertical+sqrt(1-0.5*0.5),0));
+        agregar(new BrazoHorizontal(num_cubos_horizontal));
     }
 };
 
